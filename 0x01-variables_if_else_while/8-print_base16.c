132f01d560e3883e9cc1d9861ac542f9b20539a6
freedom <freedomlesiagho@gmail.com>
2022-09-10 16:55:22 -0700

    Completed task 8

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 for success
+ */
+
+int main(void)
+{
+	int num;
+	char letter;
+
+	for (num = '0'; num <= '9'; num++)
+		putchar(num);
+
+	for (letter = 'a'; letter <= 'f'; letter++)
+		putchar(letter);
+
+	putchar('\n');
+
+	return (0);
+}
