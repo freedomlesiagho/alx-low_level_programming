be108dfe874ea277f44a8b73d120753da71070c1
freedom <freedomlesiagho@gmail.com>
2022-09-10 07:23:51 -0700

    completed task 2

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -1,4 +1,6 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
 /**
  * main - Entry point
  * Return: Always 0 (Success)
@@ -11,6 +13,7 @@ int main(void)
 	{
 		putchar(free);
 	}
+		putchar('\n');
 
 	return (0);
 }
