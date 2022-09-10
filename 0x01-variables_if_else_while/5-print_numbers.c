d7a39e01d591ef527e44dd00973eee14b92e17ee
freedom <freedomlesiagho@gmail.com>
2022-09-10 11:16:27 -0700

    completed task 5

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ * main - This is where the main coding begins
+ *
+ * Return: Will always be 0 if successful
+ */
+
+int main(void)
+{
+	int digit;
+
+	for (digit = 0; digit < 10; digit++)
+		printf("%d", digit);
+
+	printf("\n");
+
+	return (0);
+}
