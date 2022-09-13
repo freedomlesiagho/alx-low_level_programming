82d0d28c71a521da084cab6c2cc0acda6511f948
freedom <freedomlesiagho@gmail.com>
2022-09-13 16:16:01 -0700

    completed task 10

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * add - Add two numbers together
+ * @num1: first integer
+ * @num2: second integer to be added
+ * Return: sum of integers
+ */
+
+int add(int num1, int num2)
+{
+
+	return (num1 + num2);
+}
