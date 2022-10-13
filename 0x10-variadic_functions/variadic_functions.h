f9b0d87d9fe9c692840011226f37da7149e04c70
freedom <freedomlesiagho@gmail.com>
2022-10-13 06:57:35 -0700

    Completed task 1 in 0x10-variadic_functions

--- 0x10-variadic_functions/variadic_functions.h
+++ 0x10-variadic_functions/variadic_functions.h
@@ -2,17 +2,6 @@
 #define VARIADIC_FUNCTIONS_H
 
 
-/**
- * struct print - print type with corresponding print function
- * @t: print type
- * @f: print function
- */
-typedef struct print
-{
-	char *t;
-	void (*f)(va_list);
-} print_t;
-
 int _putchar(char);
 int sum_them_all(const unsigned int n, ...);
 void print_numbers(const char *separator, const unsigned int n, ...);
